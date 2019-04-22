6518108760446ad1370ad1fb47723a50c898e279
Robin Stephenson <robin.mascros@gmail.com>
2019-04-22 15:55:37 +0100

    Fixed issue where resources were not always loaded properly

--- src/main/scala/tech/robins/GitHubApi.scala
+++ src/main/scala/tech/robins/GitHubApi.scala
@@ -1,13 +1,10 @@
 package tech.robins
-import java.io.File
 
 import com.typesafe.config.ConfigFactory
 import org.kohsuke.github.GitHub
 
 object GitHubApi {
-  private val configFilePath = getClass.getClassLoader.getResource("gitHubAccount.conf").getPath
-  private val gitHubAccountConfigFile = new File(configFilePath)
-  private val config = ConfigFactory.parseFile(gitHubAccountConfigFile)
+  private val config = ConfigFactory.load(getClass.getClassLoader, "gitHubAccount.conf")
 
   val username: String = config.getString("account.name")
   val token: String = config.getString("account.oAuthToken")
